0a0819e4b3b66026d22f9b2a555cabcdf0b84ea9
ChaliasosKonstantinos <chaliasos@gmail.com>
2015-11-08 01:37:16 +0200

    Activity Rename

--- app/src/main/java/gr/compassbook/snorechat/Login.java
+++ app/src/main/java/gr/compassbook/snorechat/Login.java
@@ -3,7 +3,7 @@ package gr.compassbook.snorechat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
