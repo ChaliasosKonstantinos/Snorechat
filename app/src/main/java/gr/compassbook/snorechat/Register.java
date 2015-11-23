182ebff6cb4439a39b7feb7c5ced2143efd6e7e4
ChaliasosKonstantinos <chaliasos@gmail.com>
2015-11-23 05:37:26 +0200

    ADDED: Store user data in background Asynctask Register Redirects
    TOFIX:
    URL Encoding
    #3

--- app/src/main/java/gr/compassbook/snorechat/Register.java
+++ app/src/main/java/gr/compassbook/snorechat/Register.java
@@ -1,13 +1,52 @@
 package gr.compassbook.snorechat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    EditText etUsername, etPassword, etEmail, etCountry, etCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = (EditText) findViewById(R.id.etRegUsername);
+        etPassword = (EditText) findViewById(R.id.etRegPassword);
+        etEmail = (EditText) findViewById(R.id.etRegEmail);
+        etCountry = (EditText) findViewById(R.id.etRegCountry);
+        etCity = (EditText) findViewById(R.id.etRegCity);
     }
-}
+
+    //Creates a user class with registered details
+    public void registerUser(View view) {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String country = etCountry.getText().toString();
+        String city = etCity.getText().toString();
+
+        User userToRegister = new User(username, password, email, country, city);
+
+        ServerRequests serverRequest = new ServerRequests(this);
+        serverRequest.storeUserDataInBackgroud(userToRegister, new GetUserCallback() {
+            @Override
+            public void done(User returnedUser) {
+                showLogin();
+            }
+        });
+
+    }
+
+    private void showLogin() {
+        Toast.makeText(Register.this, "Register Successful", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this,Login.class));
+    }
+
+
+    }
--- app/src/main/java/gr/compassbook/snorechat/ServerRequests.java
+++ app/src/main/java/gr/compassbook/snorechat/ServerRequests.java
@@ -8,14 +8,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Konstantinos
@@ -48,7 +54,7 @@ public class ServerRequests {
         User user;
         GetUserCallback userCallback;
 
-        //StoreUserDataAsyncTask constructor
+        //FetchUserDataAsyncTask constructor
         public FetchUserDataAsyncTask(User userData, GetUserCallback userCallback) {
             this.user = userData;
             this.userCallback = userCallback;
@@ -132,4 +138,90 @@ public class ServerRequests {
             super.onPostExecute(returnedUser);
         }
     }
+
+    //Stores User's data on the server
+    public User storeUserDataInBackgroud(User userData, GetUserCallback userCallback) {
+        progressDialog.show();
+        new StoreUserDataAsyncTask(userData, userCallback).execute();
+        return null;
+    }
+
+    public class StoreUserDataAsyncTask extends AsyncTask<User,Void,Void> {
+
+        User user;
+        GetUserCallback userCallback;
+
+        //StoreUserDataAsyncTask constructor
+        public StoreUserDataAsyncTask(User userData, GetUserCallback userCallback) {
+            this.user = userData;
+            this.userCallback = userCallback;
+        }
+
+
+        @Override
+        protected Void doInBackground(User... params) {
+            try{
+                URL url = new URL(SERVER_ADDRESS + "register.php");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+
+                HashMap<String,String> dataToSend = new HashMap<>();
+                dataToSend.put("username", user.username);
+                dataToSend.put("password", user.password);
+                dataToSend.put("email", user.email);
+                dataToSend.put("country", user.country);
+                dataToSend.put("city", user.city);
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                writer.write(getQueryData(dataToSend));
+                writer.flush();
+                writer.close();
+                os.close();
+
+                urlConnection.connect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            userCallback.done(null);
+            super.onPostExecute(aVoid);
+        }
+    }
+
+    //Resolve dataToSend
+    public static String getQueryData(HashMap<String,String> data) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for(Map.Entry<String,String> entry:data.entrySet()) {
+            if (first) {
+                first = false;
+            } else {
+                result.append("&");
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            }
+        }
+
+        return result.toString();
+    }
+
 }
