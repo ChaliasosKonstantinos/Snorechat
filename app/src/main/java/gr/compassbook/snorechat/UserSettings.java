d7f913ba9fb65fccccab9326854103db734a6735
themis92 <themisaek21@gmail.com>
2015-12-02 18:23:30 +0200

    User Register #2

---
+++ app/src/main/java/gr/compassbook/snorechat/UserSettings.java
@@ -0,0 +1,20 @@
+package gr.compassbook.snorechat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class UserSettings extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings);
+
+    }
+
+}
